af7d3c028d52e391583cb459d5841362a88b9d82
highwoodfield <178282378+highwoodfield@users.noreply.github.com>
2024-08-27 19:11:34 +0900

    Simplify file matching

--- src/ActivityLogger.ts
+++ src/ActivityLogger.ts
@@ -4,60 +4,64 @@ import { Moment } from 'moment';
 import moment from 'moment';
 
 export class ActivityLogger {
-    app: App;
-    plugin: Plugin;
+	app: App;
+	plugin: Plugin;
 
-    constructor(app: App, plugin: DailyActivityPlugin) {
-        this.app = app;
-        this.plugin = plugin;
-    }
+	constructor(app: App, plugin: DailyActivityPlugin) {
+		this.app = app;
+		this.plugin = plugin;
+	}
 
-    private getLinks(moment: Moment, includeRegex: string[] = [], excludeRegex: string[] = [], includePaths: string[] = [], excludePaths: string[] = [], statType: 'mtime' | 'ctime'): string[] {
-        console.log(`Getting links for moment: ${moment.format()}, statType: ${statType}`);
-        return this.app.vault.getFiles()
-            .filter(f => moment.isSame(new Date(f.stat[statType]), 'day') && this.fileMatchesFilters(f.path, includeRegex, excludeRegex, includePaths, excludePaths))
-            .map(f => `[[${getLinkpath(f.path)}]]`);
-    }
+	private getLinks(moment: Moment, includeRegex: string[] = [], excludeRegex: string[] = [], includePaths: string[] = [], excludePaths: string[] = [], statType: 'mtime' | 'ctime'): string[] {
+		console.log(`Getting links for moment: ${moment.format()}, statType: ${statType}`);
+		return this.app.vault.getFiles()
+			.filter(f => moment.isSame(new Date(f.stat[statType]), 'day') && this.fileMatchesFilters(f.path, includeRegex, excludeRegex, includePaths, excludePaths))
+			.map(f => `[[${getLinkpath(f.path)}]]`);
+	}
 
-    private getLinksToFilesModifiedOnDate(moment: Moment, includeRegex: string[] = [], excludeRegex: string[] = [], includePaths: string[] = [], excludePaths: string[] = []): string[] {
-        return this.getLinks(moment, includeRegex, excludeRegex, includePaths, excludePaths, 'mtime');
-    }
+	private getLinksToFilesModifiedOnDate(moment: Moment, includeRegex: string[] = [], excludeRegex: string[] = [], includePaths: string[] = [], excludePaths: string[] = []): string[] {
+		return this.getLinks(moment, includeRegex, excludeRegex, includePaths, excludePaths, 'mtime');
+	}
 
-    private isArrayNotEmptyAndNoEmptyStrings(arr: string[]): boolean {
-        return arr.length > 0 && arr.every(item => item !== "");
-    }
+	private isArrayNotEmptyAndNoEmptyStrings(arr: string[]): boolean {
+		return arr.length > 0 && arr.every(item => item !== "");
+	}
 
-    private fileMatchesFilters(filePath: string, includeRegex: string[] = [], excludeRegex: string[] = [], includePaths: string[] = [], excludePaths: string[] = []): boolean {
-        const matches = this.isArrayNotEmptyAndNoEmptyStrings(excludeRegex) && excludeRegex.some(regex => new RegExp(regex).test(filePath)) ? false :
-            this.isArrayNotEmptyAndNoEmptyStrings(excludePaths) && excludePaths.some(part => filePath.includes(part)) ? false :
-                (includeRegex.length === 0 || includeRegex.some(regex => new RegExp(regex).test(filePath))) &&
-                (includePaths.length === 0 || includePaths.some(part => filePath.includes(part)));
-        console.log(`File ${filePath} matches filters: ${matches}`);
-        return matches;
-    }
+	private fileMatchesFilters(filePath: string, includeRegex: string[] = [], excludeRegex: string[] = [], includePaths: string[] = [], excludePaths: string[] = []): boolean {
+		if (this.isArrayNotEmptyAndNoEmptyStrings(excludeRegex) && excludeRegex.some(regex => new RegExp(regex).test(filePath))) {
+			return false;
+		}
 
-    appendLinksToContent(existingContent: string, links: string[], header: string): string {
-        return `${existingContent}\n\n${links.join('\n')}\n`;
-    }
+		if (this.isArrayNotEmptyAndNoEmptyStrings(excludePaths) && excludePaths.some(part => filePath.includes(part))) {
+			return false;
+		}
 
-    async insertTodaysModifiedFileLinks({
-                                activeView,
-                                includeRegex = [],
-                                excludeRegex = [],
-                                includePaths = [],
-                                excludePaths = []
-                            }: {
-        activeView: MarkdownView,
-        includeRegex?: string[],
-        excludeRegex?: string[],
-        includePaths?: string[],
-        excludePaths?: string[]
-    }) {
-        let content = await this.app.vault.read(activeView.file!! /* TODO */);
+		return (includeRegex.length === 0 || includeRegex.some(regex => new RegExp(regex).test(filePath))) &&
+			(includePaths.length === 0 || includePaths.some(part => filePath.includes(part)));
+	}
+
+	appendLinksToContent(existingContent: string, links: string[], header: string): string {
+		return `${existingContent}\n\n${links.join('\n')}\n`;
+	}
+
+	async insertTodaysModifiedFileLinks({
+		activeView,
+		includeRegex = [],
+		excludeRegex = [],
+		includePaths = [],
+		excludePaths = []
+	}: {
+		activeView: MarkdownView,
+		includeRegex?: string[],
+		excludeRegex?: string[],
+		includePaths?: string[],
+		excludePaths?: string[]
+	}) {
+		let content = await this.app.vault.read(activeView.file!! /* TODO */);
 		let modifiedTodayLinks: string[] = this.getLinksToFilesModifiedOnDate(
 			moment(), includeRegex, excludeRegex, includePaths, excludePaths);
 		content = this.appendLinksToContent(content, modifiedTodayLinks, 'Modified');
 
-        await this.app.vault.modify(activeView.file!! /* TODO */, content);
-    }
+		await this.app.vault.modify(activeView.file!! /* TODO */, content);
+	}
 }
